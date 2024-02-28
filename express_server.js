7b7890bf7242bdcd48b45988ba6cc73989c768d0
Lily Whitford <lilywhitford2@gmail.com>
2024-02-28 05:48:23 +0000

    add second route

--- express_server.js
+++ express_server.js
@@ -17,15 +17,22 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
-app.get('/urls.json', (req, res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('<html><body>Hello <b>World</b></body></html>\n');
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 }); 
 
-app.get('/urls', (req, res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render('urls_index', templateVars);
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+ const id = req.params.id; // access the id parameter from the route
+ const longURL = urlDatabase[id]; // look up the longURL using the id
+ const templateVars = { id: id, longURL: longURL }; // pass both values to the template
+ res.render("urls_show", templateVars); // render the urls_show template
 });
