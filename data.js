23d470daa19a6009a4e824e396031bc5609d3aa4
Lily Whitford <lilywhitford2@gmail.com>
2024-03-10 04:46:14 +0000

    modify urlDatabase to properly assess userId

--- data.js
+++ data.js
@@ -1,11 +1,11 @@
 const urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
-    userID: "aJ48lW"
+    userId: "aJ48lW"
   },
   "9sm5xK": { 
     longURL: "http://www.google.com",
-    userID: "aJ48lW"
+    userId: "aJ48lW"
   }
 };
 
